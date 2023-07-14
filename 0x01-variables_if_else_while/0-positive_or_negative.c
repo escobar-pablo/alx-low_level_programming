a636f4ea380e4b5196e32f9530c51e72676aaaf5
Ahorlu Noble <ahorlunoble7@gmail.com>
2023-07-14 09:40:45 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,22 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description: 'Checks if the number stored in n is positive or negative'
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-        int n;
+	int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /* your code goes there */
-        if(n > 0){
-                printf("%d is positive\n", n);
-        }
-        else if (n == 0){
-                printf("%d is zero\n", n);
-        }
-        else
-                printf("%d is negative\n", n);
-        return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+if (n > 0)
+	printf("%d is positive\n", n);
+else if (n == 0)
+	printf("%d is zero\n", n);
+else
+	printf("%d is negative\n", n);
+return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  * Description: 'Getting the last digit of a number'
  * Return: 0 (Success)
  */
@@ -22,7 +22,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, ld);
 	}
-	else 
+	else
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 	}
