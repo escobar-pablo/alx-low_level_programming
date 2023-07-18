2deff685a44c62647518720430bae61f625d48ef
Ahorlu Noble <ahorlunoble7@gmail.com>
2023-07-18 16:22:43 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,32 +4,31 @@
  */
 void times_table(void)
 {
-int i, j, k;
-for (i = 0; i < 10; i++)
-{
-	for (j = 0; j < 10; j++)
+int x, y, z, u, d;
+	for (x = 0; x <= 9; x++)
 	{
-		k = j * i;
-		if (j == 0)
-		{
-			_putchar(k + '0');
-		}
-		if (k < 10 && j != 0)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(k + '0');
-		}
-		else if (k >= 10)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar((k / 10) + '0');
-			_putchar((k % 10) + '0');
-		}
+	for (y = 0; y <= 9; y++)
+	{
+	z = x * y;
+	if (z > 9)
+	{
+	u = z % 10;
+	d = (z - u) / 10;
+	_putchar(44);
+	_putchar(32);
+	_putchar(d + '0');
+	}
+	else
+	{
+	if (y != 0)
+	{
+	_putchar(44);
+	_putchar(32);
+	_putchar(32);
+	}
+	_putchar(z + '0');
+	}
 	}
 	_putchar('\n');
-}
+	}
 }
