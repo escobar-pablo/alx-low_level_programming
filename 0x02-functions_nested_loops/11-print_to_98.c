b63fa783fb55f61449a1ce16b3a6336c0fad6340
Ahorlu Noble <ahorlunoble7@gmail.com>
2023-07-18 16:10:01 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,25 +7,36 @@
  */
 void print_to_98(int n)
 {
-int i, j;
-if (n <= 98)
-{
-	for (i = n; i <= 98; i++)
+	if (n <= 98)
 	{
-		if (i != 98)
-			printf("%d", i);
-		else if (i == 98)
-			printf("%d", i);
-	}
-}
-else if (n >= 98)
-{
-	for (j = n; j >= 98; j++)
+	for (; n <= 98; n++)
 	{
-		if (j != 98)
-			printf("%d", j);
-		else if (j == 98)
-			printf("%d\n", j);
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
 	}
-}
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
+	}
+	else
+	{
+	for (; n >= 98; n--)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
+	}
 }
