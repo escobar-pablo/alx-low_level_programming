d93b9b48ef2c75cf6c6cd4d12f4da582e68fe11d
Ahorlu Noble <ahorlunoble7@gmail.com>
2023-07-18 17:38:23 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,11 +11,11 @@ unsigned long fib1 = 0, fib2 = 1, sum;
 for (count = 0; count < 50; count++)
 {
 	sum = fib1 + fib2;
-	printf("%lu", sum);
+	printf("%lu ," sum);
 	fib1 = fib2;
 	fib2 = sum;
 	if (count == 49)
-		printf("\n");
+		printf('\n');
 	else
 		printf(",");
 }
