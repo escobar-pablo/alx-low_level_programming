58f2052c4101cc0049edac55a4534696ce6bc6bf
Ahorlu Noble <ahorlunoble7@gmail.com>
2023-07-18 17:07:58 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,7 +21,8 @@ int i, j, k;
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchat(' ');
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(k + '0');
 				}
 				else if (k >= 10 && k < 100)
